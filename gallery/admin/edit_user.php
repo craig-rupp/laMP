6a3394afbd7592c2a59a6aae848be70bc16ea75b
Craig Rupp <craigrupp@utexas.edu>
2017-03-01 15:48:26 -0600

    user update/delete now fully functional

--- gallery/admin/edit_user.php
+++ gallery/admin/edit_user.php
@@ -92,8 +92,8 @@
 
                       <div class="col-md-6 user_image_box">
                           
-                        <!--  <a href="#" data-toggle="modal" data-target="#photo-library"><img class="img-responsive" src="//php echo $user->image_path_and_placeholder(); " alt=""></a>
- -->
+                         <a href="#" data-toggle="modal" data-target="#photo-library"><img class="img-responsive" src="<?php echo $user->user_image_placeholder(); ?>" alt=""></a>
+
 
 
                       </div>
