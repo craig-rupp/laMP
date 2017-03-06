1d4cb3a6114ab7693c54338b2f024c5223f07786
Craig Rupp <craigrupp@utexas.edu>
2017-03-05 18:19:08 -0600

    Front-End Gallery System

--- gallery/index.php
+++ gallery/index.php
@@ -1,30 +1,42 @@
 <?php include("includes/header.php"); ?>
 
 
+<?php 
+
+    $photos = Photo::get_all_items()
+
+
+ ?>
+
+
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
 
-    
-            
-          
-         
+                <div class="thumbnails row">
+
+                    <?php foreach ($photos as $photo): ?> 
+
+                        <div class="col-xs-6 col-md-3">
+                            
+                            <a class="thumbnail" href="photo.php?id=<?php echo $photo->id;  ?>">
+                                
+                                <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+
+                            </a>
+
+                        </div>
+
+                    <?php endforeach ?>
+
+                </div>
 
             </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
 
 
 
         </div>
         <!-- /.row -->
 
-        <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
