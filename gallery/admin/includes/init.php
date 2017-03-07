ddd18570fe176431833ab91753295985e706b837
Craig Rupp <craigrupp@utexas.edu>
2017-03-06 19:13:30 -0600

    Pagination adding to home index, styled, need to add to other views however

--- gallery/admin/includes/init.php
+++ gallery/admin/includes/init.php
@@ -14,5 +14,6 @@
 	require_once(INCLUDES_PATH.DS."photo.php");
 	require_once(INCLUDES_PATH.DS."comment.php");
 	require_once(INCLUDES_PATH.DS."session.php");
+	require_once(INCLUDES_PATH.DS."paginate.php");
 
  ?>
--- gallery/index.php
+++ gallery/index.php
@@ -9,7 +9,13 @@
 
     $items_total_count = Photo::count_all();
 
-    $photos = Photo::get_all_items()
+    //$photos = Photo::get_all_items()
+    $paginate = new Paginate($page, $items_per_page, $items_total_count);
+
+    $sql = "SELECT * FROM photos ";
+    $sql .= "LIMIT {$items_per_page} ";
+    $sql .= "OFFSET {$paginate->offset()}";
+    $photos = Photo::find_this_query($sql);
 
 
  ?>
@@ -19,11 +25,11 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-12">
-
+                
                 <div class="thumbnails row">
 
                     <?php foreach ($photos as $photo): ?> 
-
+                       
                         <div class="col-xs-6 col-md-3">
                             
                             <a class="thumbnail" href="photo.php?id=<?php echo $photo->id;  ?>">
@@ -37,6 +43,35 @@
                     <?php endforeach ?>
 
                 </div>
+
+                <div class="row">
+                    
+                    <ul class="pager">
+
+                        <?php 
+
+                            if($paginate->page_total() > 1){
+                                if($paginate->has_next()){
+                                    echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a>";
+                                }
+                                for($i = 1; $i <= $paginate->page_total(); $i++){
+                                    if($i == $paginate->current_page){
+                                        echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a>";
+                                    } else {
+                                        echo "<li><a href='index.php?page={$i}'>{$i}</a>";
+                                    }
+                                }
+                            }
+                            if($paginate->page_total() > 1){
+                                if($paginate->has_previous()){
+                                    echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Prev</a>";
+                                }
+                            }
+
+                         ?>
+                    </ul>
+
+                </div>  
 
             </div>
 
