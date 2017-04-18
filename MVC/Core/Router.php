15126cfac1a9496f768e97412f766046fc9208d3
Craig Rupp <craigrupp@utexas.edu>
2017-04-17 19:42:10 -0500

    Router Class and matching query strings/routes

--- MVC/Core/Router.php
+++ MVC/Core/Router.php
@@ -2,7 +2,50 @@
 
 class Router
 {
-	
+	 /**
+	 * Associative array of routes (routing table)
+	 */
+	 protected $routes = [];
+	 protected $params = [];  
+
+	 /** 
+	 * Add a route to the routing table
+	 * 1st $route string route (The route URL)
+	 * 2nd param $params Paramerter (controller, action, etc)
+	 */
+
+	 public function add($route, $params)
+	 {
+	 	$this->routes[$route] = $params;
+	 }
+
+	 /* 
+		*Match route to the routes in the routing table, settin $params property if route is found
+		param $url is the route URL
+	 */
+
+	public function match($url)
+	{
+		foreach($this->routes as $route => $params){
+			if($url == $route){
+				echo "This {$route} matches the {$url} found in {$_SERVER['QUERY_STRING']}";
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function getParams()
+	{
+		return $this->params;
+	}
+
+
+	 public function getRoutes()
+	 {
+	 	return $this->routes;
+	 }
 }
 
  ?>
--- MVC/public/index.php
+++ MVC/public/index.php
@@ -11,5 +11,25 @@
 	require_once('../Core/Router.php');
 
 	$router = new Router();
-	echo get_class($router);
+	echo get_class($router) . "<br><br>";
+
+	/* Add the Routes */
+	$router->add('', ['controller' => 'Home', 'action' => 'index']);
+	$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+	$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+	//Display routing table
+	echo '<pre>';
+	var_dump($router->getRoutes());
+	echo '</pre>' . "<br><br>" . "Query Matches". "<br>";
+
+	//Match the Requested Route
+	$url = $_SERVER['QUERY_STRING'];
+	if($router->match($url)){
+		echo '<pre>'. '<br>';
+		var_dump($router->getParams());
+		echo '</pre>';
+	} else {
+		echo "No route found for {$url}";
+	}
  ?>
